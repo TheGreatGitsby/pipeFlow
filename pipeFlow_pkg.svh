80c3243906600de8adbcf91206ae0deb2c8be282
TheGreatGitsby <nicholas.p.stroupe@gmail.com>
2019-04-25 20:59:47 -0400

    updates

--- pipeFlow_pkg.svh
+++ pipeFlow_pkg.svh
@@ -15,10 +15,10 @@
        end \
      end \
 \
-     if ((update_regs) || (!stage_valid[0])) begin \
+     if ((update_regs) | (~stage_valid[0])) begin \
        if (rdy_switch) begin \
          `ifdef DISABLE_PIPELINING \
-           rdy_switch = !``in_name``_valid; \
+           rdy_switch <= !(``in_name``_valid & ``in_name``_rdy); \
          `endif \
          stage_valid[0] = ``in_name``_valid; \
          update_pipe[0] = ``in_name``_valid; \
@@ -30,12 +30,23 @@
 \
      end \
 \
-     if (``out_name``_rdy && ``out_name``_valid) begin \
+     if (``out_name``_rdy & ``out_name``_valid) begin \
        rdy_switch = 1; \
      end \
 \
-     ``in_name``_rdy    <= (``out_name``_rdy || !(stage_valid == '1)) && rdy_switch; \
+     ``in_name``_rdy    <= (``out_name``_rdy | ~(stage_valid == '1)) & rdy_switch; \
      ``out_name``_valid <= stage_valid[``num_stages``-1];
 
-`define pipeFlowAttrb(arg1) \
-   `define arg1
+  `define delayFlow(in_name, out_name, num_stages) \
+     static  logic [``num_stages``-1:0] stage_valid = '0; \
+\
+  stage_valid[0] <= (``in_name``_rdy  & ``in_name``_valid); \
+\
+  if (!(stage_valid[``num_stages``-1] & (!``out_name``_rdy))) begin \
+    for (int i=1; i<``num_stages``; i++) begin \
+      stage_valid[i] <= stage_valid[i-1]; \
+    end \
+  end \
+\
+  ``in_name``_rdy <= !(stage_valid | (``in_name``_rdy  & ``in_name``_valid)); \
+  ``out_name``_valid <= stage_valid[``num_stages``-1];
